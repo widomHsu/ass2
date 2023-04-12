7b838db545fba9b3444fd62ebfde92c48a38f306
mxuu0040 <mxuu0040@student.monash.edu>
2023-04-12 15:15:11 +1000

    ui 2

--- app/src/main/java/com/example/ass2/RigisterFragment.java
+++ app/src/main/java/com/example/ass2/RigisterFragment.java
@@ -59,6 +59,6 @@ public class RigisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rigister, container, false);
+        return inflater.inflate(R.layout.fragment_data_entry_screen, container, false);
     }
 }
